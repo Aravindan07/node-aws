2e9def9d0787092377fdc8fcec929889a8f558b8
Aravindan <aravindanravi33@gmail.com>
2021-10-05 14:06:30 +0530

    add: added start script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Welcome to the Homepage baby");
+	res.send("Deployed Successfully");
 });
 
 const port = process.env.port || 3333;
